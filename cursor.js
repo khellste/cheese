97b31f0a03473185812b7c93751fef87131e966c
khellste <karl.hellstern@gmail.com>
2014-09-14 23:36:15 -0700

    Added sprite support (without animation) to Cursor image

--- cursor.js
+++ cursor.js
@@ -5,19 +5,44 @@ ig.module(
 window.ch = window.ch || {};
 
 ch.Cursor = ig.Class.extend({
-	image: null,
+	anims: {},
+	animSheet: null,
+	currentAnim: null,
 	offset: { x: 0, y: 0 },
 	replace: false,
 
 	init: function (settings) {
 		ig.merge(this, settings || {});
+
+		var replace = this.replace || false;
+		Object.defineProperty(this, 'replace', {
+			enumerable: true,
+			get: function () { return replace; },
+			set: function (value) {
+				if (value === replace) return;
+				var cursor = (replace = value) ? 'none' : 'default';
+				ig.system.context.canvas.style.cursor = cursor;
+			}
+		});
+	},
+
+	addAnim: function (name, frameTime, sequence, stop) {
+		if (!this.animSheet) {
+			throw "Can't call addAnim('" + name + "') without an animSheet!";
+		}
+		var a = new ig.Animation(this.animSheet, frameTime, sequence, stop);
+		this.anims[name] = a;
+		if (!this.currentAnim) {
+			this.currentAnim = a;
+		}
+		return a;
 	},
 
 	draw: function (x, y) {
-		if (this.replace) {
-			ig.system.context.canvas.style.cursor = 'none';
+		if (!this.currentAnim) {
+			var off = this.offset, rs = ig.game._rscreen;
+			this.currentAnim.draw(x - off.x - rs.x, y - off.y - rs.y);
 		}
-		this.image && this.image.draw(x - this.offset.x, y - this.offset.y);
 	},
 
 	click: 			function () { },
